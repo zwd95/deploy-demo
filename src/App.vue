442baf104b230907c93c42e3f032bf1728432d72
zhaoweidong <zhaoweidong2017@163.com>
2022-08-18 09:43:03 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="寞寞🐶" />
+  <HelloWorld msg="寞寞🐶，是真的🐶" />
 </template>
 
 <style scoped>
